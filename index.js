d87973624247fe783a445641e34f261f76653793
Vishal Ranjan <vishal@Vishals-Mac-mini.local>
2025-05-11 16:26:57 +0530

    Initial commit -sayHello API on port 80

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 80;
+
+app.get('/sayHello', (req, res) => {
+  res.json({ message: 'Hello User' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
